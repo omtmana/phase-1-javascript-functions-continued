14bb54d574c0ce55a928455f3ecf043eb7256bc3
Olivia <oliviatmanalastas@gmail.com>
2022-07-05 10:08:08 -0400

    commit tree 77a05b12d00d3efbc5a74b266114762b926c5c83

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+let saturdayFun = (satFun = 'roller-skate') => {
+    return `This Saturday, I want to ${satFun}!`
+}
+
+let mondayWork = (monWork = 'go to the office') => {
+    return `This Monday, I will ${monWork}.`
+}
+
+let wrapAdjective = () => {
+    let innerAdjective = (innerAdj = "special") => {
+        return `You are ${innerAdj}`
+        let secondIn = (secInn = "*") => {
+            wrapAdjective("%")("A dedicated programmer");
+        }
+    }
+}
